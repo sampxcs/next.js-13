a1c7d53a794fd110617144529d8cd498323797a4
Ian Rosales <irosales@distritomoda.com.ar>
2023-05-09 10:57:25 -0300

    last commit

--- app/(showcase)/post/[id]/[gifId]/@aside/page.tsx
+++ app/(showcase)/post/[id]/[gifId]/@aside/page.tsx
@@ -7,10 +7,15 @@ export default async function Page ({ params }:any) {
   const postContent = await getPostById(id)
   const user = await getUserById(postContent.userId)
 
+  const post = {
+    ...postContent,
+    user
+  }
+
   return (
     <div className='flex flex-col gap-4 p-4 bg-light-black rounded-2xl'>
       {/* @ts-expect-error Async Server Component */}
-      <Profile {...user} />
+      <Profile {...post} />
     </div>
   )
 }
