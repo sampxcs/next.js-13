3ef068f389c5677d70d3745d759f78882e7a5d70
Ian Rosales <irosales@distritomoda.com.ar>
2023-05-09 11:07:39 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage () {
   return (
     <>
       <section className='flex flex-col gap-16 items-center p-8 w-full h-[70vh]'>
-        <h1 className='text-4xl text-center mb-16'>
+        <h1 className='text-4xl text-center mb-10'>
           Next<small>.js</small> 13 <span className='dark:text-neutral-400'>&#183;</span> <span className='font-semibold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>Tailwind.css</span>
         </h1>
         <h2 className='text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-500'>
@@ -17,7 +17,7 @@ export default function HomePage () {
         <p className='text-center dark:text-neutral-400'>Used by some of the world's largest companies, Next.js enables you to create <strong className='dark:text-white'>full-stack Web applications</strong> by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.</p>
         <div className='flex flex-col gap-2 items-center'>
           <div className='flex gap-4'>
-            <ButtonPrimary href='/posts'>Get Started</ButtonPrimary>
+            <ButtonPrimary href='/feed'>Get Started</ButtonPrimary>
             <ButtonSecundary href='https://nextjs.org/' target='_blank'>Lear Next.js</ButtonSecundary>
           </div>
           <div className={`${code.className} text-sm px-2 py-1 rounded-md bg-opacity-5 dark:bg-opacity-20 shadow-2xl bg-black`}> ~ npx create-next-app@latest</div>
